37b232e687c9ccb0676865619a1ae1c27d9063a4
arv <alini.vicente@yahoo.com.br>
2024-10-07 00:19:03 -0300

    feat: add bcript to generate the password hash

--- api/routes/public.js
+++ api/routes/public.js
@@ -1,12 +1,31 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient()
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-     const user = req.body;
-     console.log(user);
-    res.status(201).json(user);
-});
+router.post('/signup', async (req, res) => {
+    try {
+        const user = req.body;
 
+        const salt = await bcrypt.genSalt(10);
+
+        const hashPassword = await bcrypt.hash(user.password, salt);
+
+        const userdb = await prisma.user.create({
+            data: {
+                email: user.email,
+                name: user.name,
+                password: hashPassword
+            }
+
+        })
+        res.status(201).json(userdb);
+    } catch (err) {
+        res.status(500).json('Error no servidor, tente mais tarde!');
+
+    }
+});
 
 export default router;
