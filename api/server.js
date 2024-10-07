7cf43c71b0ded643753f9c11632102cc79918b73
arv <alini.vicente@yahoo.com.br>
2024-10-07 00:16:27 -0300

    fix: middleware express.json()

--- api/server.js
+++ api/server.js
@@ -2,15 +2,10 @@ import express from 'express';
 import publicRouter from './routes/public.js';
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
 app.use('/', publicRouter);
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
-
-//SzOtVC2MTFps2fjV
-//arvic
-
-//
