c5ef8ba9e343172f79130b90098a3b4030b74f3b
arv <alini.vicente@yahoo.com.br>
2024-10-07 07:42:27 -0300

    feat: add login route and set security token

--- api/routes/public.js
+++ api/routes/public.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 const router = express.Router();
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/signup', async (req, res) => {
     try {
@@ -27,5 +29,41 @@ router.post('/signup', async (req, res) => {
 
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const userInfo = req.body;
+
+        //find user in database
+        const user = await prisma.user.findUnique({
+            where: { email: userInfo.email },
+        })
+
+        // check if the user exists
+        if (!user) {
+            res.status(404).json('Usuário não encontrado!');
+        }
+
+        //validate that the password is correct
+        const isMatch = await bcrypt.compare(userInfo.password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json('Senha inválida!')
+        }
+
+        //generate token
+        const payload ={
+            id: user.id,
+            name: user.name
+        };
+        const token = jwt.sign(payload, JWT_SECRET,{expiresIn:'1m'})
+        
+        res.status(200).json(token);
+
+    } catch (err) {
+        res.status(500).json('Error no servidor, tente mais tarde!');
+    }
+
+})
 
 export default router;
