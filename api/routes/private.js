c2cfb6fd81c72191a7af05ad24667e35ac61471f
arv <alini.vicente@yahoo.com.br>
2024-10-08 08:26:03 -0300

    feat: add dice routes

---
+++ api/routes/private.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { PrismaClient } from '@prisma/client';
+import DiceController from '../controllers/DiceController.js';
+
+const router = express.Router();
+const prisma = new PrismaClient();
+const diceController = new DiceController();
+
+router.get('/home', async (req, res) => {
+    try {
+        const user = await prisma.user.findMany()
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json('Error no servidor, tente mais tarde!');
+
+    }
+});
+router.get('/roll', diceController.roll);
+
+router.get('/roll-modified/:modifiers', diceController.rollModified);
+
+
+export default router
