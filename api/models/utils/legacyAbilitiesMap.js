59dc91cfec614b332d92d6398c5eaaf205036eb1
arv <alini.vicente@yahoo.com.br>
2024-10-09 12:18:37 -0300

    feat: add character class

---
+++ api/models/utils/legacyAbilitiesMap.js
@@ -0,0 +1,88 @@
+const legacyAbilitiesMap = {
+    Alraunes: {
+        ability1: 'Filhos de Darnawel',
+        ability2: 'Essência Revigorante'
+    },
+    Elfos: {
+        ability1: 'Sabedoria Antiga',
+        ability2: 'Pensamento Célere'
+    },
+    Minotauros: {
+        ability1: 'Nômade',
+        ability2: 'Visão Etérea'
+    },
+    Netunes: {
+        ability1: 'Adaptação dos Oceanos',
+        ability2: 'Recompensa da Caçada'
+    },
+    Anoes: {
+        ability1: 'Herança dos Três Irmãos',
+        ability2: 'Fortitude Ampliada'
+    },
+    Daevas: {
+        ability1: 'Essência Abissal',
+        ability2: 'Pacto Originário'
+    },
+    Ursars: {
+        ability1: 'Virtude do Primeiro Povo',
+        ability2: 'Presente de Norduk'
+    },
+    Valdraks: {
+        ability1: 'Resiliência',
+        ability2: 'Incansável'
+    },
+    Humanos: {
+        ability1: 'Adaptabilidade',
+        ability2: 'Determinação'
+    },
+    Delahks: {
+        ability1: 'Versatilidade',
+        ability2: 'Miragem'
+    },
+    Draenus: {
+        ability1: 'Presente das Sombras',
+        ability2: 'Manto Noturno'
+    },
+    Kitaris: {
+        ability1: 'Mobilidade Felina',
+        ability2: 'Sorte'
+    },
+    Orkrashs: {
+        ability1: 'Agraciado pelos Rituais',
+        ability2: 'Fúria dos Antigos'
+    },
+    Inaris: {
+        ability1: 'Instinto Natural',
+        ability2: 'Sempre Alerta'
+    },
+    Seikos: {
+        ability1: 'Vontade das Brumas',
+        ability2: 'Espelho da Alma'
+    },
+    Yuansus: {
+        ability1: 'Dinastia Narzepiana',
+        ability2: 'Poder Oculto'
+    },
+    Zaokans: {
+        ability1: 'Corpo Fechado',
+        ability2: 'Furor das Brumas'
+    },
+    Elementais: {
+        ability1: 'Essência Elemental',
+        ability2: 'Encarnação Elemental'
+    },
+    Forjados:{
+        ability1: 'Armadura Viva',
+        ability2:'Corpo de Ferro'
+    },
+    Kahatszas:{
+        ability1: 'Conectado com o Véu',
+        ability2:'Magia Antiga',
+    },
+    Vennelis:{
+        ability1: 'Resplendor',
+        ability2:'Toque da Salvação',
+    }
+};
+
+export default legacyAbilitiesMap;
