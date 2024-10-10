0cb4869feeb524ae4c7553ebcb981a77a26ee05f
arv <alini.vicente@yahoo.com.br>
2024-10-10 15:25:25 -0300

    feat: adds new character routes

--- api/routes/private.js
+++ api/routes/private.js
@@ -8,21 +8,32 @@ const prisma = new PrismaClient();
 const diceController = new DiceController();
 const  characterController = new CharacterController();
 
-
+//teste
 router.get('/home', async (req, res) => {
     try {
-        const user = await prisma.user.findMany()
+        const user = await prisma.user.findMany();
         res.status(200).json(user);
     } catch (err) {
         res.status(500).json('Error no servidor, tente mais tarde!');
 
     }
 });
+
 router.get('/roll', diceController.roll);
 
-router.get('/roll-modified/:modifiers', diceController.rollModified);
+router.get('/roll/modified/:modifiers', diceController.rollModified);
 
-router.post('/character',characterController.teste)
+router.post('/character',characterController.createCharacter);
+
+router.get('/character/find/:name',characterController.getCharacter);
+
+router.get('/character/findAll',characterController.getAllCharacters);
+
+router.put('/characters/update-parameter/:name', characterController.updateCharacterParameter);
+
+router.put('/characters/update-knowledge/:name', characterController.updateCharacterKnowledge);
+
+router.delete('/characters/delete/:name', characterController.deleteCharacter);
 
 
-export default router
+export default router;
