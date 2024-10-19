53b9869bf0beaecfd94b70d001b04cc67d74ec5e
arv <alini.vicente@yahoo.com.br>
2024-10-19 11:59:21 -0300

    feat: add a method to find all sheet

--- api/controllers/SheetsController.js
+++ api/controllers/SheetsController.js
@@ -65,6 +65,26 @@ class SheetsController {
     }
   };
 
+  async getAllSheets(req, res) {
+    const userId = req.userId;
+    try {
+      const sheets = await prisma.sheet.findMany({
+        where: {
+          userId: userId,
+        },
+      });
+
+      if (sheets.length === 0) {
+        return res.status(404).json({ message: 'No sheets found for this user.' });
+      }
+      res.status(200).json(sheets);
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'An error occurred while fetching sheets.' });
+    }
+  };
+
 }
 
 export default SheetsController;
