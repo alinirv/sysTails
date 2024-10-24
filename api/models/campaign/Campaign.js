5e4a09bfe29eb95310f41b4838ecc4bf38277663
arv <alini.vicente@yahoo.com.br>
2024-10-23 22:32:59 -0300

    refactor:  refactor campaign class

--- api/models/campaign/Campaign.js
+++ api/models/campaign/Campaign.js
@@ -1,13 +1,22 @@
 class Campaign {
-
-    constructor(name, description = '', sheets = [], masterName) {
+    constructor(name, description, masterName, status = 'OPEN', sheets=[]) {
         this.name = name;
         this.description = description;
-        this.sheets = sheets;
         this.masterName = masterName; 
-
-    
+        this.status = status;
+        this.sheets = sheets; 
+        this.token = this.generateCampaignToken()
     }
-}
 
+    generateCampaignToken() {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        const length = 5;
+        for (let i = 0; i < length; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          result += characters[randomIndex];
+        }
+        return result;
+      }
+}
 export default Campaign;
