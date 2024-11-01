19d6234e151b0e6ebb5377a5453a4670e885fe4d
arv <alini.vicente@yahoo.com.br>
2024-10-31 22:49:02 -0300

    feat(app): implements the home page and style settings

--- app/src/pages/home/index.jsx
+++ app/src/pages/home/index.jsx
@@ -1,9 +1,31 @@
-function Home() {
-    return (
-        <div >
-            <h1>Welcome to Tails</h1>
-        </div>
-    )
-}
+import React from 'react';
+import imagem from '../../assets/10091.png'
 
-export default Home
+const Home = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-slate-900 to-teal-700">
+            <div class="bg-slate-950 text-white max-w-screen-lg w-full px-4 py-6 rounded-lg items-center" >
+                <div className="px-10">
+                    <img className="mx-auto rounded-lg shadow-lg p-8" src={imagem} alt="Logo do jogo Gaia" />
+                </div>
+
+                <p class="text-lg mb-4">
+                    Gaia: O Prelúdio é um projeto nacional de RPG de alta fantasia medieval, trazendo um sistema moderno e simplificado, que oferece uma experiência de jogo fluída, onde cada personagem é único e a palavra-chave é liberdade.
+                </p>
+                <p class="text-lg mb-4">
+                    Com um sistema híbrido de criação de personagens, regras e um combate simplificado, Gaia: O Prelúdio tem o objetivo de ser um sistema acessível para toda a comunidade, tanto para aqueles que procuram uma aventura mais simples e rápida, como para aqueles que procuram uma jornada épica e repleta de escolhas.
+                </p>
+                <p class="text-lg mb-4">
+                    Sendo aqui que entra o Tails! Como seu melhor companheiro, ele está aqui para você desfrutar desse sistema em poucos cliques, com rápidas criações de personagens, seleções de poderes e magias, além de um gerenciamento geral para mestres e jogadores.
+                </p>
+                <div class="flex justify-center py-5">
+                    <button class="w-3/6 bg-teal-600 text-slate-900 font-semibold py-2 rounded-md hover:none">
+                        Comece Agora
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
