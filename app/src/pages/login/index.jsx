a753689d16a1a2c8b5933bf27520113bc7bf6d0e
arv <alini.vicente@yahoo.com.br>
2024-11-01 08:44:32 -0300

    feat(app): implements login page

--- app/src/pages/login/index.jsx
+++ app/src/pages/login/index.jsx
@@ -1,7 +1,40 @@
-function Login(){
-    return(
-        <div className="login">
-            <h2>Login</h2>
+import { Link } from 'react-router-dom';
+import Footer from '../../components/footer/Footer';
+
+function Login() {
+    return (
+        <div>
+            <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-slate-900 to-teal-700">
+                <div className="bg-slate-950 rounded-lg shadow-lg p-8 max-w-md w-full">
+                    <h2 className="text-2xl font-bold text-center text-white mb-6">Login</h2>
+                    <form className="space-y-4">
+                       
+                        <input
+                            placeholder="Email"
+                            type="email"
+                            className="w-full p-3 border border-slate-800 rounded-md focus:outline-none focus:ring-2 focus:none bg-transparent"
+                        />
+                        <input
+                            placeholder="Senha"
+                            type="password"
+                            className="w-full p-3 border border-slate-800 rounded-md focus:outline-none focus:ring-2 focus:none bg-transparent"
+                        />
+                        <button
+                            type="submit"
+                            className="w-full bg-teal-600 text-slate-950 font-semibold py-2 rounded-md hover:none transition duration-200"
+                        >
+                            Entrar
+                        </button>
+                    </form>
+                    <p className="mt-4 text-center text-gray-600">
+                        Ainda não possui uma conta?{' '}
+                        <Link to='/signup' className="text-white hover:underline">
+                            Increva-se
+                        </Link>
+                    </p>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 
