23215b5c71ee60397793e5057ff7fed8c17a27b6
arv <alini.vicente@yahoo.com.br>
2024-11-02 07:46:18 -0300

    feat(api): install cors dependencies

--- api/server.js
+++ api/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import publicRouter from './routes/public.js';
 import privateRouter from './routes/private.js'
 import auth from "./middleware/auth.js"
+import cors  from 'cors'
+
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/', publicRouter);
 app.use('/', auth, privateRouter);
 
