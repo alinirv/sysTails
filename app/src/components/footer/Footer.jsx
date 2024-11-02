d6f2cc8cfbce43befbb70eaa5ab0970a08521d27
arv <alini.vicente@yahoo.com.br>
2024-11-02 07:16:21 -0300

    style(app): add logo

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -4,10 +4,16 @@ import SocialIcons from "./SocialIcons";
 const Footer = () => {
   return (
     <footer className="bg-gray-950 text-white">
+      <div className="1 sm:grid-cols-2 lg:grid-cols-3 gap-10
+      text-center pt-2 text-gray-400 text-sm pb-8">
+        <p>Gaia: O Prelúdio é um produto da Auroria – Game Studio e seus respectivos criadores, todos os direitos reservados.</p>
+      </div>
+      
       <div
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
       text-center pt-2 text-gray-400 text-sm pb-8"
       >
+        
         <span>© 2024 Arvic. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
         <SocialIcons />
