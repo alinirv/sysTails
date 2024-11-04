e526a13c423ae3e4a653c68df6da6b71a0415739
arv <alini.vicente@yahoo.com.br>
2024-11-04 07:41:28 -0300

    feat(api): includes character data search when searching for a sheet

--- api/controllers/SheetsController.js
+++ api/controllers/SheetsController.js
@@ -37,14 +37,14 @@ class SheetsController {
       });
       return res.status(201).json(newSheet);
     } catch (error) {
-      console.error('Erro ao criar ficha:', error);
-      return res.status(500).json({ message: 'Erro ao criar ficha', error });
+      return res.status(500).json('Erro ao criar ficha');
     }
   };
 
   async getSheet(req, res) {
     const { name } = req.params;
     const userId = req.userId;
+    const userName= req.userName;
     try {
       const sheet = await prisma.sheet.findFirst({
         where: {
@@ -57,10 +57,27 @@ class SheetsController {
         return res.status(400).json('Ficha não encontrada, verifique o nome da ficha!');
 
       }
-      res.status(200).json(sheet);
+      const character = await prisma.character.findUnique({
+        where: {
+          id: sheet.characterId,
+        },
+      });
+  
+      if (!character) {
+        return res.status(400).json('Personagem associado à ficha não encontrado!');
+      }
+  
+      // Combinar os dados da ficha e do personagem
+      const completeSheet = {
+        ...sheet,
+        character,
+        player: userName
+      };
+  
+      res.status(200).json(completeSheet);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'An error occurred while fetching the sheet.' });
+      res.status(500).json('An error occurred while fetching the sheet.');
       throw error;
     }
   };
@@ -75,13 +92,13 @@ class SheetsController {
       });
 
       if (sheets.length === 0) {
-        return res.status(404).json({ message: 'No sheets found for this user.' });
+        return res.status(404).json('No sheets found for this user.');
       }
       res.status(200).json(sheets);
 
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'An error occurred while fetching sheets.' });
+      res.status(500).json('An error occurred while fetching sheets.');
     }
   };
 
@@ -101,13 +118,13 @@ class SheetsController {
             id: sheet.id
           }
         });
-        return res.status(200).json({ message: 'sheet deleted successfully.' });
+        return res.status(200).json('sheet deleted successfully.' );
       } else {
-        return res.status(404).json({ message: 'sheet not found.' });
+        return res.status(404).json('sheet not found.' );
       }
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'An error occurred while updating the sheet parameter.' });
+      res.status(500).json('An error occurred while updating the sheet parameter.');
     }
   };
 
@@ -139,12 +156,12 @@ class SheetsController {
         });
         return res.status(200).json(updatedSheet);
       } else {
-        return res.status(404).json({ message: 'sheet not found.' });
+        return res.status(404).json('sheet not found.');
       }
 
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'An error occurred while updating the sheet parameter.' });
+      res.status(500).json('An error occurred while updating the sheet parameter.');
     }
   };
 
